36166aca7b1345ecea522de48295f67e0314abdd
alizaRabe <127301907+alizaRabe@users.noreply.github.com>
2024-04-22 00:30:07 +0800

    commit

---
+++ functions/api.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const router = require("./routes/author");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const dbCloudUrl = 
+'mongodb+srv://alizarabe:5514200307@webprog.sbgwzrn.mongodb.net/server?retryWrites=true&w=majority&appName=Webprog';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(dbCloudUrl)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
